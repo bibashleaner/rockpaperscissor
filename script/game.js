e089e69d78b6bcf8d6df3e266e1a527f6129d4e2
bibashlearner <bibashchaudhary330@gmail.com>
2024-05-22 00:28:10 +0545

    added button css

--- script/game.js
+++ script/game.js
@@ -2,6 +2,31 @@
 
 function getComputerChoise(){
     let array = ['rock', 'paper', 'scissor'];
-    let choise = math.floor(math.random(array) * array.length);
-    console.log(choise);
+    let choose = math.floor(math.random(array) * array.length);
+    return choose;
 }
+
+function getHumanchoise(){
+
+}
+
+//Image occurance interval
+
+// var randomNumber = 0;
+// let time = setInterval(function(){
+//     const image = document.getElementById('image');
+
+//     var pic = [
+//         'paper.png',
+//         'rock.png',
+//         'scissor.png'
+//     ]  
+    
+//     if(randomNumber>2){
+//         return randomNumber=0;
+//     }
+
+//     image.src = pic[randomNumber];
+//     randomNumber++;
+
+// },300);~
